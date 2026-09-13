aaf4e499b6fec132a4fe5992bf88e3636796ae87
Nive <niknive7@gmail.com>
2026-09-13 19:31:34 +0530

    Update portfolio deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "@fontsource/geist-sans/400.css"; // Specific weight
-import "@fontsource/geist-sans/700.css"; // Bold
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/500.css";
+import "@fontsource/geist-sans/700.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
